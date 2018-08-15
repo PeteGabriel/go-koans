dcd4540384593f0cfcf81f025cb9440503593b7f
Pedro Gabriel <pedrogabriel@protonmail.ch>
2018-08-15 22:38:27 +0100

    Add impl for concurrency, channels and panic test cases.

--- about_concurrency.go
+++ about_concurrency.go
@@ -12,20 +12,31 @@ func isPrimeNumber(possiblePrime int) bool {
 func findPrimeNumbers(channel chan int) {
 	for i := 2; ; /* infinite loop */ i++ {
 		// your code goes here
+		if isPrimeNumber(i) {
+			channel <- i
+		}
 
 		assert(i < 100) // i is afraid of heights
+		if i == 99 {
+			break
+		}
 	}
 }
 
 func aboutConcurrency() {
 	ch := make(chan int)
 
-	assert(__delete_me__) // concurrency can be almost trivial
+	assert(len(ch) == 0) // concurrency can be almost trivial
 	// your code goes here
+	go func(){
+		findPrimeNumbers(ch)
+	}()
 
 	assert(<-ch == 2)
 	assert(<-ch == 3)
 	assert(<-ch == 5)
 	assert(<-ch == 7)
 	assert(<-ch == 11)
+
+	defer close(ch)
 }
