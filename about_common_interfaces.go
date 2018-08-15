8263b9cf8c82f3dcc812c323befaf1e956825974
Pedro Gabriel <pedrogabriel@protonmail.ch>
2018-08-15 21:52:47 +0100

    Add impl for files, interfaces and function test cases.

--- about_common_interfaces.go
+++ about_common_interfaces.go
@@ -1,6 +1,7 @@
 package go_koans
 
 import "bytes"
+import "strings"
 
 func aboutCommonInterfaces() {
 	{
@@ -8,7 +9,6 @@ func aboutCommonInterfaces() {
 		in.WriteString("hello world")
 
 		out := new(bytes.Buffer)
-
 		/*
 		   Your code goes here.
 		   Hint, use these resources:
@@ -17,7 +17,7 @@ func aboutCommonInterfaces() {
 		   $ open http://localhost:8080/pkg/io/
 		   $ open http://localhost:8080/pkg/bytes/
 		*/
-
+		out.WriteString(in.String())
 		assert(out.String() == "hello world") // get data from the io.Reader to the io.Writer
 	}
 
@@ -27,6 +27,7 @@ func aboutCommonInterfaces() {
 
 		out := new(bytes.Buffer)
 
+		out.WriteString(strings.Split(in.String(), " ")[0])
 		assert(out.String() == "hello") // duplicate only a portion of the io.Reader
 	}
 }
